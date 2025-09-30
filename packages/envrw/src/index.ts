80f82b3eb5f43dec29074ad89974e0ff65d2d17b
Alex Gorischek <alex.gorischek@gmail.com>
2025-09-30 07:48:53 -0700

    Remove envrw

--- packages/envrw/src/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { EnvSource } from "./EnvSource";
-
-export const source = (filePath: string) => new EnvSource(filePath);
-
-export { EnvSource };
-export type { EnvUnlisten } from "./EnvSource";
-export { formatEnvContent, formatEnvFile } from "./formatEnv";
