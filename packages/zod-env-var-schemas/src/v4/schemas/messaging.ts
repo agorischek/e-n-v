42b5ec3d174b91c90951696e09072dd3e6f5cbfd
Alex Gorischek <alex.gorischek@gmail.com>
2025-10-13 18:23:40 +0000

    Renames

---
+++ packages/zod-env-var-schemas/src/v4/schemas/messaging.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { descriptions, messages, patterns } from "../../shared/infrastructure";
+
+export const rabbitmqUrl = () =>
+  z
+    .string()
+    .describe(descriptions.rabbitmqUrl)
+    .regex(patterns.rabbitmqUrl, {
+      message: messages.rabbitmqUrlFormat,
+    });
+
+export const RABBITMQ_URL = rabbitmqUrl();
