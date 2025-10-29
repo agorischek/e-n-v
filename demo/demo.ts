097b56fa220d430c8371eaa529e92ebcdc0977d7
Alex Gorischek <alex.gorischek@gmail.com>
2025-10-28 17:13:37 -0700

    Add dev deps

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 import { prompt, schemas } from "../packages/env/src";
 const { PORT, NODE_ENV, API_KEY } = schemas;
 
@@ -6,5 +5,4 @@ await prompt({
   path: ".env",
   root: import.meta.url,
   schemas: { PORT, NODE_ENV, API_KEY },
-  channel: { dotenv },
 });
